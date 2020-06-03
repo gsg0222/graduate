cc5fe00d0929e6d81f48ad7299aa8f62723c2103
TSUCHIYA Shunsuke <gsg0222.1977@gmail.com>
2020-06-03 15:24:24 +0900

    404ページ追加

---
+++ src/404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Inquiry Page.
+ *
+ * @package WP
+ */
+
+?>
+<?php
+get_header();
+get_template_part( 'content-sub-header' );
+?>
+	<section>
+		<p class="title">Page Not Found.</p>
+	</section>
+
+<?php
+get_template_part( 'content-news' );
+?>
+<?php
+get_footer();
